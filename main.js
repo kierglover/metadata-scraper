edd855ed76c9f2fe094f4a04d5533d5142bc061c
Kieran <kier.p.glover@gmail.com>
2021-10-15 14:11:55 +0700

    :sparkles: Placeholder csv creation

--- main.js
+++ main.js
@@ -9,13 +9,41 @@ const metascraper = require('metascraper')([
     require('metascraper-title')(),
     require('metascraper-url')()
   ])
+
+  const createCsvWriter = require('csv-writer').createObjectCsvWriter;
   
   const got = require('got')
   
-  const targetUrl = 'http://www.bloomberg.com/news/articles/2016-05-24/as-zenefits-stumbles-gusto-goes-head-on-by-selling-insurance'
+  const targetUrl = 'https://arimagenomics.com/'
   
   ;(async () => {
     const { body: html, url } = await got(targetUrl)
     const metadata = await metascraper({ html, url })
     console.log(metadata)
   })()
+
+
+  //Create CSV
+  const csvWriter = createCsvWriter({
+      path: '/Users/imac/Developer/metadata-scraper/output/output.csv',
+      header: [
+          {id: 'name', title: 'NAME'},
+          {id: 'lang', title: 'LANGUAGE'}
+      ]
+  });
+   
+  const records = [
+      {name: 'Bob',  lang: 'French, English'},
+      {name: 'Mary', lang: 'English'}
+  ];
+   
+  csvWriter.writeRecords(records)       // returns a promise
+      .then(() => {
+          console.log('...Done');
+      });
+   
+  // This will produce a file path/to/file.csv with following contents:
+  //
+  //   NAME,LANGUAGE
+  //   Bob,"French, English"
+  //   Mary,English
